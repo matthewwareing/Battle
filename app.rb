b358541a5d0699889b4b04854546f19d7d84bf53
Matthew <mdwareing@gmail.com>
2018-05-31 14:15:01 +0100

    sorry

--- app.rb
+++ app.rb
@@ -1,11 +1,14 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+    enable :sessions
+
     get '/' do
         erb :index
     end
 
     post '/names' do
+        session[:p1_name]
         @name_1 = params['p1_name']
         @name_2 = params['p2_name']
         erb :play
